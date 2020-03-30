6dffe6526e1338dd0358380bc6df535304729650
Soybean <soybean3r@gmail.com>
2020-03-30 12:53:42 -0400

    Update 3
    Update Code -
    + added comment for randomizing
    + playerCount text field deletes after pcButton is pressed
    + added distinction between each word
    + separated create player function to other file
    + auto spacing adjustment
    
    CSS Update -
    + Font Adjustment for Canvas

--- scripts/button.js
+++ scripts/button.js
@@ -22,8 +22,11 @@ let nmbButton = function () {
   pcButton.value = 'submit';
   pcButton.addEventListener('click', () => {
     Game.playerCount = parseInt(findID('text-input').value);
+
+    //self delete
     pcButton.parentNode.removeChild(pcButton);
-    inputText.value = '';
+    inputText.parentNode.removeChild(inputText);
+
     if (typeof Game.playerCount !== 'number') {
       mainText.innerHTML = 'Please input a number.'
     } else {
@@ -32,29 +35,19 @@ let nmbButton = function () {
       create('textarea', 'word-1', 'word-1-space');
       create('textarea', 'word-2', 'word-2-space');
       create('textarea', 'word-3', 'word-3-space');
+
       findID('word-1').value = '';
-      findID('word-2').value = '';
-      findID('word-3').value = '';
+      findID('word-1-space').innerHTML = 'Word 1 - ' + findID('word-1-space').innerHTML;
+      findID('word-2-space').value = '';
+      findID('word-2-space').innerHTML = 'Word 2 - ' + findID('word-2-space').innerHTML;
+      findID('word-3-space').value = '';
+      findID('word-3-space').innerHTML = 'Word 3 - ' + findID('word-3-space').innerHTML;
+
       create('button', 'word-submit', 'canvas');
       findID('word-submit').value = 'submit';
       // Create Players
-      for (let i = 0; i < Game.playerCount; i++) {
-        Game.players[i] = new Player();
-      }
-      // Ask for word input
-      for (let player in Game.players) {
-
-        mainText.innerHTML = `\nPlayer ${Number(player)+1}, enter a single word ${i + 1}:\n`;
-        wordInput = capitalize(wordInput);
-        Game.wordBank.push(wordInput);
-        Game.players[player].bank.push(wordInput);
-      }
-      Game.rando(Game.wordBank);
-      Game.rando(Game.wordBank);
-      Game.rando(Game.players);
-      Game.rando(Game.players);
+      createPlayers();
     }
   });
   inputText.value = 0;
-
 }
--- scripts/game.js
+++ scripts/game.js
@@ -1,18 +1,37 @@
 // Introduction
 findID('main-text').innerHTML = 'Let\'s Play!'
 
-create('button','play','canvas')
+create('button', 'play', 'canvas')
 
 findID('play').value = 'Click to play!';
-findID('play').addEventListener('click', ()=>{
+findID('play').addEventListener('click', () => {
   // delete this button
-  findID('play').parentNode.removeChild( findID('play') );
+  findID('play').parentNode.removeChild(findID('play'));
   // direct player
   mainText.innerHTML = 'How Many Players?';
   //create a textbox for input
-  create('textarea','text-input','canvas');
+  create('textarea', 'text-input', 'canvas');
   findID('text-input').type = 'number';
   nmbButton();
 });
 
 
+//Create players
+let createPlayers = function () {
+  for (let i = 0; i < Game.playerCount; i++) {
+    Game.players[i] = new Player();
+  }
+  // Ask for word input
+  for (let player in Game.players) {
+
+    mainText.innerHTML = `\nPlayer ${Number(player)+1}, enter a single word ${i + 1}:\n`;
+    wordInput = capitalize(wordInput);
+    Game.wordBank.push(wordInput);
+    Game.players[player].bank.push(wordInput);
+  }
+  // Randomize word bank and player
+  Game.rando(Game.wordBank);
+  Game.rando(Game.wordBank);
+  Game.rando(Game.players);
+  Game.rando(Game.players);
+}
